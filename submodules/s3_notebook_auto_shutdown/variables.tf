2e5d5855e7ead273688b43eae68bfcc7b4adffb7
Nabeel Hamad <n.hamad@hotmail.co.uk>
2025-02-09 20:04:16 +0000

    renamed s3 bucket folder to s3_sagemaker_template

--- submodules/s3_notebook_auto_shutdown/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "kms_arn" {
-  description = "kms key to encrypt EFS"
-  type        = string
-}
--- submodules/s3_sagemaker_template/s3_upload.tf
+++ submodules/s3_sagemaker_template/s3_upload.tf
@@ -2,12 +2,12 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-resource "aws_s3_bucket" "sagemaker_template" {
-  bucket = "lwf-sagemaker-storage-${data.aws_region.current.name}-${data.aws_caller_identity.current.account_id}"
+resource "aws_s3_bucket" "sagemaker_output_bucket" {
+  bucket = "sagemaker-storage-bucket-${data.aws_region.current.name}"
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "sagemaker_template" {
-  bucket = aws_s3_bucket.sagemaker_template.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "sagemaker_output_bucket" {
+  bucket = aws_s3_bucket.sagemaker_output_bucket.bucket
   rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = var.kms_arn
@@ -17,15 +17,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "sagemaker_templat
 }
 
 resource "aws_s3_bucket_public_access_block" "block_public_auto_shutdown" {
-  bucket                  = aws_s3_bucket.sagemaker_template.id
+  bucket                  = aws_s3_bucket.sagemaker_output_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "sagemaker_template_life_cycle_config" {
-  bucket = aws_s3_bucket.sagemaker_template.id
+resource "aws_s3_bucket_lifecycle_configuration" "sagemaker_output_bucket_life_cycle_config" {
+  bucket = aws_s3_bucket.sagemaker_output_bucket.id
 
   rule {
     id = "life-cycle-configuration-rule"
@@ -36,27 +36,25 @@ resource "aws_s3_bucket_lifecycle_configuration" "sagemaker_template_life_cycle_
   }
 }
 
-resource "aws_s3_bucket_logging" "sagemaker_template_access_log" {
-  bucket = aws_s3_bucket.sagemaker_template.id
-
-  target_bucket = aws_s3_bucket.sagemaker_template.id
-  target_prefix = "log/"
-}
-
-resource "aws_s3_bucket_versioning" "sagemaker_template_versioning" {
-  bucket = aws_s3_bucket.sagemaker_template.id
+resource "aws_s3_bucket_versioning" "sagemaker_output_bucket_versioning" {
+  bucket = aws_s3_bucket.sagemaker_output_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
+resource "aws_s3_object" "autoshutdown_tar_upload" {
+  bucket     = aws_s3_bucket.sagemaker_output_bucket.id
+  key        = "notebook_timer/sagemaker_studio_autoshutdown-0.1.5.tar.gz"
+  source     = "${path.module}/../../assets/auto_shutdown_template/sagemaker_studio_autoshutdown-0.1.5.tar.gz"
+  kms_key_id = var.kms_arn
+  source_hash = filemd5("${path.module}/../../assets/auto_shutdown_template/sagemaker_studio_autoshutdown-0.1.5.tar.gz")
+}
+
 resource "aws_s3_object" "template_uploader" {
-  bucket     = aws_s3_bucket.sagemaker_template.id
+  bucket     = aws_s3_bucket.sagemaker_output_bucket.id
   key        = "sagemaker_template/template.yml"
   source     = "${path.module}/../../assets/sagemaker_template/template.yml"
   kms_key_id = var.kms_arn
-
-  # Checks hash to enusre if file is changed, it's updated in S3
-  source_hash       = filemd5("${path.module}/../../assets/sagemaker_template/template.yml")
+  source_hash = filemd5("${path.module}/../../assets/sagemaker_template/template.yml")
 }
-
